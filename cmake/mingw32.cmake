6c006fb8179f6c70e1d0ba390caf25b35d9243aa
iregviettel-coder <iregviettel@gmail.com>
2026-05-13 10:58:50 +0700

    Create mingw32.cmake

---
+++ cmake/mingw32.cmake
@@ -0,0 +1,15 @@
+# cmake/mingw32.cmake
+# Cross-compile toolchain for MinGW i686 (32-bit Windows DLL)
+# Usage: cmake -B build -DCMAKE_TOOLCHAIN_FILE=cmake/mingw32.cmake
+
+set(CMAKE_SYSTEM_NAME Windows)
+set(CMAKE_SYSTEM_PROCESSOR i686)
+
+set(CMAKE_C_COMPILER   i686-w64-mingw32-gcc)
+set(CMAKE_CXX_COMPILER i686-w64-mingw32-g++)
+set(CMAKE_RC_COMPILER  i686-w64-mingw32-windres)
+
+set(CMAKE_FIND_ROOT_PATH /usr/i686-w64-mingw32)
+set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
